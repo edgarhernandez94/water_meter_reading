a212d78a13c32fb1b816c5837d8abf2084546c90
Hugojair18 <hugoherrera1800@gmail.com>
2024-07-10 19:52:07 +0900

    picturess

--- Test.py
+++ Test.py
@@ -9,13 +9,13 @@ def detect_water_meter_value(image_path):
     # Convertir la imagen a escala de grises
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
-    # Recortar la región de interés (ROI) que contiene los números
-    x_start, x_end = 280, 380
-    y_start, y_end = 250, 320
+    # Recortar la región de interés (ROI) que contiene los números en la parte roja
+    x_start, x_end = 150, 230
+    y_start, y_end = 90, 110
     roi = gray[y_start:y_end, x_start:x_end]
     
     # Aplicar un filtro Gaussiano para reducir el ruido
-    roi = cv2.GaussianBlur(roi, (5, 5), 0)
+    roi = cv2.GaussianBlur(roi, (9, 9), 0)
     
     # Aplicar umbralización adaptativa para mejorar el contraste
     thresh = cv2.adaptiveThreshold(
@@ -46,25 +46,19 @@ def detect_water_meter_value(image_path):
         return "No se pudo detectar el valor del medidor de agua."
 
 # Ruta a la imagen del medidor de agua
-image_path = 'C:/Users/ATR/Downloads/imagemeter1.jpg'  # Ajusta la ruta de la imagen
+image_path = 'C:/Users/ATR/Desktop/Water_meter/Dataset/captured_image_20240710_165955.jpg'  # Ajusta la ruta de la imagen
 
 # Detectar el valor del medidor de agua
 resultado = detect_water_meter_value(image_path)
 print(resultado)
 
-# Mostrar la imagen procesada (opcional)
+# Mostrar la imagen procesada
 image = cv2.imread(image_path)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-roi = gray[250:320, 280:380]  # Ajusta estos valores según sea necesario
+roi = gray[75:95, 165:215]
 thresh = cv2.adaptiveThreshold(
     roi, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)))
 plt.imshow(thresh, cmap='gray')
-plt.title('ROI del Medidor de Agua Procesado')
+plt.title('ROI del Medidor de Agua Procesado (Parte Roja Ajustada)')
 plt.show()
-
-
-
-##########################################################################################
-# Se necesita crear dataset de imagenes para probar
-# Se necesita homogenizar y generar el dispositivo para poder estar tomando fotos constantemente y visualizarlas en plataforma web.
